1abe5c8e6e732fff4a052a70ea2d40dc5551f1b8
Nezar Abdennur <nabdennur@gmail.com>
2014-02-16 12:46:30 -0500

    pep8

--- bin/run_cell.py
+++ bin/run_cell.py
@@ -11,7 +11,8 @@ cf = find_connection_file(cf_name)
 
 # FIXME: this gets the wrong pid (the connected client's)
 # We want the pid of the kernel to send interrupts to.
-# One hacky approach is to ask the kernel to os.getpid(), assuming it's responsive (see vim-ipython).
+# One hacky approach is to ask the kernel to os.getpid(),
+# assuming it's responsive (see vim-ipython).
 pid = int(re.findall(r'kernel-(\d+)\.json', cf)[0])
 
 # Connect to kernel
@@ -21,7 +22,7 @@ client = km.client()
 client.start_channels()
 
 # Propagate SIGTERM from sublime as SIGINT to kernel
-def interrupt_handler(signum, frame): 
+def interrupt_handler(signum, frame):
     os.kill(pid, signal.SIGINT)
     sys.exit(130)
 signal.signal(signal.SIGTERM, interrupt_handler)
@@ -39,13 +40,10 @@ prompt = reply['content']['execution_count']
 if status == 'ok':
     print 'Out [%s]: succeeded!\n' % prompt
 
-elif status == 'error':        
+elif status == 'error':
     sys.stderr.write('Err [%s]: failed!\n' % prompt)
     for line in reply['content']['traceback']:
         sys.stderr.write(line)
     #print reply['content']['ename']
     #print reply['content']['evalue']
 sys.exit(0)
-
-
-
